d080138c976c57b3e480bec62c92c7f046df48d2
BPlotinskiy <boris_plotinskii@mail.ru>
2022-10-06 14:20:41 +0700

    решена задача 4

---
+++ Less1/HW/task4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a=155;
+int b=8;
+int c = 10;
+int max=a;
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.ReadLine();
