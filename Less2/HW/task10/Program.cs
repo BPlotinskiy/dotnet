9c3a71de15e67e30d2efd302172f484c48d52ab2
BPlotinskiy <boris_plotinskii@mail.ru>
2022-10-11 15:28:34 +0700

    решена задача 10 и 13

---
+++ Less2/HW/task10/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите метод, который принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите число : ");
+int n;
+n = int.Parse(Console.ReadLine());
+
+if (n > 100 && n < 999)
+{
+Console.Write(Metod(n));
+}
+else
+{
+Console.Write("Вы ввели не трехзначное число"); 
+};
+
+int Metod(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}
---
+++ Less2/HW/task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите метод, который выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число : ");
+int n;
+n = int.Parse(Console.ReadLine());
+
+if (n > 100 && n < 999)
+{
+Console.Write(Metod(n));
+}
+else
+{
+Console.Write("Вы ввели не трехзначное число"); 
+};
+
+int Metod(int a)
+{
+    
+    int result = ((a / 1) % 10);
+    return result;
+}
