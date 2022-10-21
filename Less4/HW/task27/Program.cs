c681749cedfea48b03fcd8b1b6de2debd88b36ed
BPlotinskiy <boris_plotinskii@mail.ru>
2022-10-21 13:28:32 +0700

    задача 27

---
+++ Less4/HW/task27/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.Write("Введите Число: ");
+int n;
+n = int.Parse(Console.ReadLine());
+void sum (int n)
+{
+int sum = 0;
+   while (n>0)
+   {
+     int dig = n%10;
+     sum = sum + dig;
+     n = n/10;
+   }
+   Console.WriteLine(sum);
+}
+sum(n);
