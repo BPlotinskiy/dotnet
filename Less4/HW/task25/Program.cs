43639762ca662b160b6415f18bf5e96a272be41d
BPlotinskiy <boris_plotinskii@mail.ru>
2022-10-21 17:25:49 +0700

    задача 25

---
+++ Less4/HW/task25/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+// опишите метод, который принимает на вход два числа (A и B) и возводит число A в целую степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+//без math.pow
+Console.Write("Введите Число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите Число B: ");
+int b = int.Parse(Console.ReadLine());
+void step (int a, int b)
+{
+    int i = 1;
+    int step =a;
+    while (i<b)
+        {
+        step=step*a;
+        i=i+1;
+        }
+    Console.WriteLine(step);
+
+}
+step(a,b);
+
+
+
