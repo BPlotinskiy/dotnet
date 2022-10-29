8e9f74d64baf96ee93a55ca052393fa3043276a5
BPlotinskiy <boris_plotinskii@mail.ru>
2022-10-29 18:01:49 +0700

    задача 36

---
+++ Less5/HW/task36/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+void PrinArray ( int [] arra)
+{
+for (int i = 0; i<arra.GetLength(0); i++)
+{
+Console.WriteLine(arra[i]);
+} 
+}
+void FillArray(int [] arra)
+{
+    for (int i = 0; i<arra.GetLength(0); i++)
+{
+    arra [i] = new Random().Next(1,10);
+
+} 
+}
+void Summ (int [] arra)
+{
+    int sum = 0;
+    for (int i = 1; i<arra.GetLength(0); i=i+2)
+    {
+        sum = sum+arra[i];
+    }
+    Console.WriteLine(sum);
+}
+int [] array = new int [4];
+FillArray (array);
+PrinArray (array);
+Summ (array);
