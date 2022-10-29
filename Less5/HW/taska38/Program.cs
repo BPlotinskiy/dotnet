17fac7667bf4426a0f1e4c40eba116e09bafbaa8
BPlotinskiy <boris_plotinskii@mail.ru>
2022-10-29 18:37:55 +0700

    задача 38

---
+++ Less5/HW/taska38/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+void PrinArray ( int [] arra)
+{
+for (int i = 0; i<arra.GetLength(0); i++)
+{
+Console.WriteLine(arra[i]);
+} 
+}
+void FillArray(int [] arra)
+{
+    for (int i = 0; i<arra.GetLength(0); i++)
+{
+    arra [i] = new Random().Next(10,100);
+
+} 
+}
+void Raznic (int [] arra)
+{
+    int raznica = 0;
+    int max = arra [0];
+    int min = arra [0];
+    for (int i = 0; i<arra.GetLength(0); i=i+1)
+    {
+        if ( max < arra[i]) max = arra [i];
+        if ( min > arra[i]) min = arra [i];
+    }
+    raznica = max-min;
+    Console.WriteLine(raznica);
+}
+int [] array = new int [4];
+FillArray (array);
+PrinArray (array);
+Raznic (array);
